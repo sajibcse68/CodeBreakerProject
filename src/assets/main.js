59e5548f5c26e9bae9bb2203fcc7418098a5393c
Sajib <sajib@appscode.com>
2017-02-08 12:15:02 +0600

    Done Code Breaker Game With JavaScript

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,83 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if(answer.value === '' || attempt.value === '') {
+        setHiddenFields();
+    }
+    if (!validateInput(input)) {
+        return false;
+    }
+    attempt.value += 1;
+
+    if (getResults(input.value)) {
+        setMessage('You Win! :)');
+        showAnswer(true);
+        showReplay();
+    } else if (attempt.value >= 10) {
+        setMessage('You Lose! :(');
+        showAnswer(false);
+        showReplay();
+    } else {
+        setMessage('Incorrect, try again.');
+    }
 }
 
-//implement new functions here
+function showAnswer(success) {
+    let code = document.getElementById('code');
+    if (success) {
+      code.className += ' success';
+    } else {
+        code.className += ' failure'
+    }
+    code.innerHTML = answer.value;
+}
+
+function showReplay() {
+    document.getElementById('guessing-div').style.display = 'none';
+    document.getElementById('replay-div').style.display = 'block';
+}
+
+function getResults(input) {
+    let match = true;
+    let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    const len = input.length;
+    for (let i = 0; i < len; i++) {
+      if (input.charAt(i) === answer.value.charAt(i)) {
+          html += '<span class="glyphicon glyphicon-ok"></span>';
+          correct++;
+      } else if (answer.value.indexOf(input.charAt(i)) > -1) {
+          html += '<span class="glyphicon glyphicon-transfer"></span>';
+          match = false;
+      } else {
+          html += '<span class="glyphicon glyphicon-remove"></span>';
+          match = false;
+      }
+    }
+    html += '</div></div>';
+    document.getElementById('results').innerHTML = html;
+
+    if (match) {
+        return true;
+    }
+    return false;
+}
+
+function setHiddenFields() {
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    while(answer.length < 4) {
+        answer.value += '0' + answer.value;
+    }
+    attempt.value = 0;
+}
+
+function setMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input) {
+    if (input.length === 4) {
+        return true;
+    }
+    setMessage('Guesses must be exactly 4 characters long.');
+    return false;
+}
